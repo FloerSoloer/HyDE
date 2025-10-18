d3b1e1287fa76d4e88428ea7ced2f659b95bbaea
FloerSoloer <floer.solo.03@gmail.com>
2025-10-18 20:30:44 +0800

    helix: add C-z bind to zsh for resuming helix

--- Configs/.config/zsh/functions/helix.zsh
+++ Configs/.config/zsh/functions/helix.zsh
@@ -1,3 +1,13 @@
 if command -v "helix" &>/dev/null; then
   alias hx='helix'
+
+  # Allow Ctrl-z to toggle between suspend and resume
+  function Resume {
+    fg
+    zle push-input
+    BUFFER=""
+    zle accept-line
+  }
+  zle -N Resume
+  bindkey "^Z" Resume
 fi

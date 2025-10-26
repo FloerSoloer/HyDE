d1050fcb46b60c24c3851325967c37e5c977965d
FloerSoloer <floer.solo.03@gmail.com>
2025-10-26 12:57:24 +0800

    binds: remove focuswindow, simplify workspace binds

--- Configs/.local/lib/hyde/focuswindow.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-aworkspace="$(hyprctl activeworkspace -j | jq .id)"
-aaddress="$(hyprctl clients -j | jq -r "map(select(.workspace.id == $aworkspace))[$1].address")"
-hyprctl dispatch focuswindow "address:$aaddress"
